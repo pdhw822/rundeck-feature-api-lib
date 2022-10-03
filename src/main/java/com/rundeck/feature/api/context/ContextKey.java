85fc546f9aac4dd6dce2d070dc76bec934418a55
Stephen Joyner <stephen@rundeck.com>
2022-10-03 12:34:22 -0500

    Simplify context

---
+++ src/main/java/com/rundeck/feature/api/context/ContextKey.java
@@ -0,0 +1,6 @@
+package com.rundeck.feature.api.context;
+
+public enum ContextKey {
+    ACTION_DATA,
+    FEATURE_CONFIG
+}
--- src/main/java/com/rundeck/feature/api/context/FeatureActionContext.java
+++ src/main/java/com/rundeck/feature/api/context/FeatureActionContext.java
@@ -5,7 +5,7 @@ import com.rundeck.feature.api.event.ActionEventPublisher;
 /**
  * An instance of this class is created for every action that is executed
  * withing the system. If the action will receive data, this class will
- * containt that data in the key 'action-data'. Any other supporting classes
+ * contain that data in the key 'action-data'. Any other supporting classes
  * that will be needed by the action will be put into the context by the class
  * that is responsible for invoking the FeatureAction's execute() method.
  *
@@ -14,11 +14,8 @@ import com.rundeck.feature.api.event.ActionEventPublisher;
  *
  */
 public interface FeatureActionContext {
-    public static final String KEY_ACTION_DATA = "action-data";
-    public static final String KEY_FEATURE_CONFIG = "feature-config";
     String getActionId();
-    void setActionId(String id);
-    void put(String key, Object contextObject);
-    <T> T get(String key, Class<T> objectType);
+    String getUser();
+    <T> T get(ContextKey key, Class<T> objectType);
     ActionEventPublisher getEventPublisher();
 }
