9841e6bfe55a59ed0848d3994fa0b762d1f486fa
Stephen Joyner <stephen@rundeck.com>
2022-10-03 16:04:37 -0500

    Add context keys class

---
+++ src/main/java/com/rundeck/feature/api/context/ContextKeys.java
@@ -0,0 +1,6 @@
+package com.rundeck.feature.api.context;
+
+public class ContextKeys {
+    public static final String ACTION_DATA = "action-data";
+    public static final String FEATURE_CONFIG = "feature-config";
+}
